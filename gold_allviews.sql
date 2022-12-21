1d22b88d99bf09aa2b7284566e4ed43e7aebfa74
Tunay Tokmak <100707983+tokmak-tunay@users.noreply.github.com>
2022-12-21 11:30:47 +0100

    add gold_allviews

--- gold_allviews.sql
+++ gold_allviews.sql
@@ -4,10 +4,10 @@ CREATE TABLE tokmaktunay_homework.gold_allviews
           parquet_compression = 'SNAPPY',
           external_location = 's3://tokmak.tunay/datalake/gold_allviews'
     ) AS SELECT
-    title as article,
+    article,
     sum(views) as total_top_view,
     max(rank) as top_rank,
     count(date) as   ranked_days
          FROM tokmaktunay_homework.silver_views 
-         GROUP BY title
+         GROUP BY article
  
